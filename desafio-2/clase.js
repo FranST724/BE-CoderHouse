2637338d1d387cfd20b203bd1ddc37a150f8340a
FranST724 <fran.tasinazzo@gmail.com>
2022-04-28 18:49:32 -0300

    Desafio-2

--- desafio-2/clase.js
+++ desafio-2/clase.js
@@ -1,16 +1,74 @@
-const fs = require('fs');
+import * as fs from 'fs';
 
 class Contenedor {
 	constructor(file) {
 		this.file = file;
 	}
 
-	async save() {
+	async save(objeto) {
 		try {
 			const data = await fs.promises.readFile(this.file, 'utf-8');
-			return JSON.parse(data);
+			const prevData = JSON.parse(data);
+			const IdCount = prevData.length + 1;
+			const newObject = { ...objeto, id: IdCount };
+			prevData.push(newObject);
+			console.log(prevData);
+			await fs.promises.writeFile(`./${this.file}`, `${JSON.stringify(prevData)}`);
+			return IdCount;
 		} catch (error) {
 			console.log('error', error);
+		}
+	}
+
+	async getById() {
+		try {
+			const data = await fs.promises.readFile(this.file, 'utf-8');
+			const prevData = JSON.parse(data);
+			const productId = prevData.find((producto) => producto.id === id);
+			if (productId === undefined) {
+				console.log(null);
+			} else {
+				console.log(productId);
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	}
+
+	async getAll() {
+		try {
+			const data = await fs.promises.readFile(this.file, 'utf-8');
+			const prevData = JSON.parse(data);
+			console(prevData);
+		} catch (err) {
+			console.log(err);
+		}
+	}
+
+	async deleteById(id) {
+		try {
+			const data = await fs.promises.readFile(this.file, 'utf-8');
+			const prevData = JSON.parse(data);
+			const productId = prevData.find((producto) => producto.id === id);
+			if (productId) {
+				const productDelete = prevData.filter((producto) => producto.id !== id);
+				await fs.promises.writeFile(`./${this.file}`, `${JSON.stringify(productDelete)}`);
+				console.log('objeto eliminado exitosamente');
+			} else {
+				console.log('No hay un producto para eliminar');
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	}
+
+	async deleteAll() {
+		try {
+			const deleteAll = [];
+			await fs.promises.writeFile(`./${this.file}`, `${JSON.stringify(deleteAll)}`);
+			console.log('todos los objetos fueron eliminados exitosamente');
+		} catch (err) {
+			console.log(err);
 		}
 	}
 }
--- desafio-2/test.js
+++ desafio-2/test.js
@@ -1,4 +1,14 @@
 import Contenedor from './clase.js';
 
-let contenedor = new Contenedor('./productos.txt');
-contenedor.save();
+const main = async () => {
+	let contenedor = new Contenedor('./productos.txt');
+	await contenedor.save({
+		title: 'Regla',
+		price: 200
+	});
+	await contenedor.getById();
+	await contenedor.getAll();
+	await contenedor.deleteById();
+	await contenedor.deleteAll();
+};
+main();
